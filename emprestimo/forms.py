e0ac9f4ab9c57662e010361034fff2c6cb3e538d
Mary <146475589+maryvitoria002@users.noreply.github.com>
2026-08-23 13:52:21 -0300

    Criei os forms de emprestimo, ainda não foi testado

--- emprestimo/forms.py
+++ emprestimo/forms.py
@@ -1,3 +1,14 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Emprestimo
+
+class CriarEmprestimoForm(ModelForm):
+    class Meta:
+        model = Emprestimo
+        fields = ['livro', 'cliente', 'data_emprestimo', 'data_devolucao', 'bibliotecario']
+
+class EditarEmprestimoForm(ModelForm):
+    class Meta:
+        model = Emprestimo
+        fields = ['livro', 'cliente', 'data_emprestimo', 'data_devolucao', 'bibliotecario']
+
