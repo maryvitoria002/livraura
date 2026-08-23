f543226577f3f90fe2f9c986d1698caa8529c441
Mary <146475589+maryvitoria002@users.noreply.github.com>
2026-08-22 22:25:01 -0300

    Melhorei o app livro

--- livro/models.py
+++ livro/models.py
@@ -4,19 +4,30 @@ class Autor(models.Model):
     nome = models.CharField(max_length=100)
     biografia = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.nome
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.nome
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(blank=True, null=True)
-    isbn = models.CharField(max_length=20, unique=True)
     disponivel = models.BooleanField(default=True)
     autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
-    editora = models.CharField(max_length=100, blank=True, null=True)
+    categoria = models.ManyToManyField(Categoria, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
     def __str__(self):
         return self.titulo
