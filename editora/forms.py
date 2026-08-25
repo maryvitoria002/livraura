00a61f3adc665d907aa19baba694272276400dd4
Mary <146475589+maryvitoria002@users.noreply.github.com>
2026-08-24 23:47:58 -0300

    Ajeitei uns erros e fiz os templates básicos

--- editora/forms.py
+++ editora/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Editora
 
 
-class LivroForm(forms.ModelForm):
+class EditoraForm(forms.ModelForm):
     class Meta:
         model = Editora
 
--- editora/views.py
+++ editora/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Editora
+from .forms import EditoraForm
 
 def criar(request):
-    if request.method == "POST":
-        nome = request.POST.get("nome")
-        editora = Editora(nome=nome)
-        editora.save()
+    form = EditoraForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
         return redirect("editora_listar")
-
-    return render(request, "editora/criar.html")
+    
+    return render(request, "editora/criar.html", {"form": form})
 
 def listar(request):
     editoras = Editora.objects.all()
@@ -20,14 +21,13 @@ def detalhar(request, editora_id):
 
 def editar(request, editora_id):
     editora = get_object_or_404(Editora, id=editora_id)
+    form = EditoraForm(request.POST or None, instance=editora)
 
-    if request.method == "POST":
-        nome = request.POST.get("nome")
-        editora.nome = nome
-        editora.save()
+    if form.is_valid():
+        form.save()
         return redirect("editora_listar")
-
-    return render(request, "editora/editar.html", {"editora": editora})
+    
+    return render(request, "editora/editar.html",{"form":form})
 
 def deletar(request, editora_id):
     editora = get_object_or_404(Editora, id=editora_id)
