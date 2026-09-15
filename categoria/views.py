ce5008dabbb649d44ceabc3e5cdaa1a09d6b7a45
kel <146475381+kelvinstanley@users.noreply.github.com>
2026-09-15 13:40:20 -0300

    Add permission checks for categoria views

--- categoria/views.py
+++ categoria/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria
 from .forms import CategoriaForm
-from  django.contrib.auth.decorators import login_required
+from  django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
+@permission_required("categoria.add_categoria")
 def criar(request):
     form = CategoriaForm(request.POST or None)
     
@@ -14,16 +15,19 @@ def criar(request):
     return render(request, "categoria/criar.html", {"form": form})
 
 @login_required
+@permission_required("categoria.view_categoria")
 def listar(request):
     categorias = Categoria.objects.all()
     return render(request, "categoria/listar.html", {"categorias": categorias})
 
 @login_required
+@permission_required("categoria.view_categoria")
 def detalhar(request, categoria_id):
     categoria = get_object_or_404(Categoria, id=categoria_id)
     return render(request, "categoria/detalhar.html", {"categoria": categoria})
 
 @login_required
+@permission_required("categoria.change_categoria")
 def editar(request, categoria_id):
     categoria = get_object_or_404(Categoria, id=categoria_id)
     form = CategoriaForm(request.POST or None, instance=categoria)
@@ -35,6 +39,7 @@ def editar(request, categoria_id):
     return render(request, "categoria/editar.html",{"form":form})
 
 @login_required
+@permission_required("categoria.delete_categoria")
 def deletar(request, categoria_id):
     categoria = get_object_or_404(Categoria, id=categoria_id)
     categoria.delete()
